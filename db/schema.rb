e880009d0b211ed7d21ee737b90226771f79d22d
taiki.satou <satotaijj@gmail.com>
2020-04-08 13:01:57 +0000

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_075852) do
+ActiveRecord::Schema.define(version: 2020_04_08_121600) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "status"
   end
 
 end
